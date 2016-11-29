54f63e02e0809b033d39fec369039b35fdc266c2
Ihor Peresunko <marazmatic77@gmail.com>
2016-11-29 23:56:41 +0200

    A little improvements

--- test_app/app.py
+++ test_app/app.py
@@ -3,19 +3,45 @@ import falcon
 import pymongo
 
 
-class MongoQueryBlueprint(object):
+class BaseMongo(object):
     """docstring for ."""
     def __init__(self):
         client = pymongo.MongoClient('localhost', 27017)
         db = client['test-data']
         self.collection = db['processing_status_urls']
 
+
+class Completed(BaseMongo):
+    """docstring for ."""
+    def __init__(self):
+        super(Completed, self).__init__()
+
     def on_get(self, req, resp):
         completed = self.collection.find({'status': 'COMPLETED'})
         result = list(completed)
-        resp.body = dumps(result)
+        data = {
+            'count': len(result),
+            'results': result
+        }
+        resp.body = dumps(data)
+        resp.status = falcon.HTTP_200
+
+class Skipped(BaseMongo):
+    """docstring for ."""
+    def __init__(self):
+        super(Skipped, self).__init__()
+
+    def on_get(self, req, resp):
+        completed = self.collection.find({'status': 'SKIPPED'})
+        result = list(completed)
+        data = {
+            'count': len(result),
+            'results': result
+        }
+        resp.body = dumps(data)
         resp.status = falcon.HTTP_200
 
 
 api = application = falcon.API()
-api.add_route('/completed', MongoQueryBlueprint())
+api.add_route('/completed', Completed())
+api.add_route('/skipped', Skipped())
