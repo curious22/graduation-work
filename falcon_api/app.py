b2d30ed75f99a659451fb730c0d33e4a56f7008f
Ihor Peresunko <marazmatic77@gmail.com>
2017-01-21 21:45:32 +0200

    Allowed CORS requests

--- falcon_api/app.py
+++ falcon_api/app.py
@@ -1,5 +1,7 @@
 import falcon
+from falcon_cors import CORS
 from api import search
 
-api = application = falcon.API()
+cors = CORS()
+api = application = falcon.API(middleware=[cors.middleware])
 api.add_route('/api/search', search.Search())
