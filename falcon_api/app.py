953f42a548970befaba787eddb54124917945c5b
Ihor Peresunko <marazmatic77@gmail.com>
2017-01-21 21:51:34 +0200

    Added a required option

--- falcon_api/app.py
+++ falcon_api/app.py
@@ -2,6 +2,6 @@ import falcon
 from falcon_cors import CORS
 from api import search
 
-cors = CORS()
+cors = CORS(allow_all_origins=True)
 api = application = falcon.API(middleware=[cors.middleware])
 api.add_route('/api/search', search.Search())
